755af0c2437422d4a405ad324205b8e511f94123
Rioba <nyakeriphilip2@gmail.com>
2023-06-20 18:00:29 +0300

    all

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+/*Declear a functio custom_putchar*/
+void custom_putchar(char c)
+{
+	write(1, &c, 1);
+}
+int main(void)
+{
+	char text[] = "_putchar";
+	int i = 0;
+
+	while (text[i] != '\0')
+	{
+		custom_putchar(text[i]);
+		i++;
+	}
+	return (0);
+}
