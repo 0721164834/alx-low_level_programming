d961fbfddcdfe9f6c5171933ff4cc82741aef69a
Rioba <nyakeriphilip2@gmail.com>
2023-06-22 16:53:42 +0300

    all

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if intenger is postive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	postive_or_nagative(i);
+
+	return (0);
+}
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void postive_or_negative(int i);
+
+#endif /*MAIN_H*/
+
