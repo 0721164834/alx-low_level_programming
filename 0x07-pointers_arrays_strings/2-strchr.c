3e1c96e1caab23f3d30f394feca5e39fd399d1e2
Rioba <nyakeriphilip2@gmail.com>
2023-07-04 17:54:55 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -18,5 +17,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return (NULL);
+	return ('\0');
 }
