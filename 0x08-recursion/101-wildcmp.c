dcf2050b02f090c1e25abc5158aa8b3654878212
Rioba <nyakeriphilip2@gmail.com>
2023-07-06 17:50:35 +0300

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,13 +15,9 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (1);
 		}
-		while (*s1 != '\0')
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 		{
-			if (wildcmp(s1, s2 + 1))
-			{
-				return (1);
-			}
-			s1++;
+			return (1);
 		}
 		return (wildcmp(s1, s2 + 1));
 	}
