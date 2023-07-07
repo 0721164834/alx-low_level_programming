ac94140f1ef9e497e08b8fd6f88b33c0f46a93f2
Rioba <nyakeriphilip2@gmail.com>
2023-07-07 22:10:55 +0300

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: int
+ * @argv: string
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
