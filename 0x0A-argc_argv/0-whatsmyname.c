4c41614cb73d558aa4f7c2d447bdedbcec7f5928
Rioba <nyakeriphilip2@gmail.com>
2023-07-07 22:44:03 +0300

    argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * main - check the code
@@ -9,6 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
