dc5c8a5d26ee8c84dbd8b738d7b89527d4d9578d
Rioba <nyakeriphilip2@gmail.com>
2023-07-11 15:11:08 +0300

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,91 +1,40 @@
 #include "main.h"
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 /**
  * strtow - split a string
  * @str: string
  *
- * Return: NULL if str == NULL || str == ""
+ * Return: NULL if str == 0 || *str == ' '
  */
 char **strtow(char *str)
 {
-	/*count the number of words*/
-	int wc = 0;
-	int i = 0;
-	int len = strlen(str);
+	int i, j, k, word_count = 0;
 	char **words;
-	int wi = 0;
-	int start;
-	int end;
-	int wl;
-	char *word;
-	int j;
 
-	if (str == NULL || *str == '\0')
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
-	while (i < len)
-	{
-		/*skip leading space*/
-		while (i < len && str[i] == ' ')
-		{
-			i++;
-		}
-		/*count a word if it begins with a non-space character*/
-		if (i < len && str[i] != ' ')
-		{
-			wc++;
-			/*skip the rest of the word*/
-			while (i < len && str[i] != ' ')
-			{
-				i++;
-			}
-		}
-	}
-	if (wc == 0)
-	{
-		return (NULL);
-	}
-	/*allocate memory for the array of words*/
-	words = (char **)malloc((wc + 1) * sizeof(char *));
+	for (i = 0; str[i]; i++)
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			word_count++;
+	words = malloc(sizeof(char *) * (word_count + 1));
 	if (words == NULL)
-	{
 		return (NULL);
-	}
-	/*split the string into words*/
-	i = 0;
-	while (i < len)
+	j = 0;
+	for (i = 0; i < word_count; i++)
 	{
-		/*skip leading space*/
-		while (i < len && str[i] == ' ')
-		{
-			i++;
-		}
-		/*allocate memory for a word*/
-		start = i;
-		while (i < len && str[i] != ' ')
-		{
-			i++;
-		}
-		end = i - 1;
-		wl = end - start + 1;
-		word = (char *)malloc((wl + 1) * sizeof(char));
-		if (word == NULL)
-		{
-			/*free the previously allocated memory*/
-			for (j = 0; j < wi; j++)
-			{
-				free(words[j]);
-			}
-			free(words);
+		while (str[j] == ' ')
+			j++;
+		for (k = 0; str[j + k] != ' ' && str[j + k]; k++)
+			;
+		words[i] = malloc(sizeof(char) * (k + 1));
+		if (words[i] == NULL)
 			return (NULL);
-		}
-		/*copy the word from the string to the allocated memory*/
-		strncpy(word, &str[start], wl);
-		word[wl] = '\0';
-		words[wi] = word;
-		wi++;
+
+		strncpy(words[i], str + j, k);
+		words[i][k] = '\0';
+		j += k;
 	}
-	words[wi] = NULL;
+	words[i] = NULL;
 	return (words);
 }
