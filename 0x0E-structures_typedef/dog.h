4aa9cae69fad3c5be46073b8c5e79c3c164d51c4
Rioba <nyakeriphilip2@gmail.com>
2023-07-17 14:56:31 +0300

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - header name
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
