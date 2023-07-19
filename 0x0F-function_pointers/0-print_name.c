62725b2d3887c1ccb221184062e81909b5188627
Rioba <nyakeriphilip2@gmail.com>
2023-07-19 21:29:56 +0300

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,10 @@
  * print_name - this function prints a name
  * @name: name
  * @f: f
- * Return:
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	/*Call the function pointer with the name as an argument*/
 	if (name != NULL && f != NULL)
 		f(name);
 }
