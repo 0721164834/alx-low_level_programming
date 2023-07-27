2854ab39446294b87a50b90c0fcf2ca863b10389
Rioba <nyakeriphilip2@gmail.com>
2023-07-27 12:41:35 +0300

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len -Returns the number of elements in a list_t list
+ * @h: Pointer to the head of the list.
+ * Return: The number of nodes in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
