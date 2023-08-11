dffd5eb61d9afee28f5d6520feabe8d34acd98c3
Rioba <nyakeriphilip2@gmail.com>
2023-08-11 21:36:01 +0300

    100

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,4 +9,22 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+typedef struct
+{
+	unsigned char e_ident[16];
+	uint16_t e_type;
+	uint16_t e_machine;
+	uint32_t e_version;
+	uint64_t e_entry;
+	uint64_t e_phoff;
+	uint64_t e_shoff;
+	uint32_t e_flags;
+	uint16_t e_ehsize;
+	uint16_t e_phentsize;
+	uint16_t e_phnum;
+	uint16_t e_shentsize;
+	uint16_t e_shnum;
+	uint16_t e_shstrndx;
+} Elf64_Ehdr;
 #endif
