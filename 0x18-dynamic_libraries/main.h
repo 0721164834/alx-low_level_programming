76f6ab1f895c320b9a08b752ddfd8c85c5292b4c
Rioba <nyakeriphilip2@gmail.com>
2024-03-25 23:18:01 +0300

    dynamic libraries

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int _isupper(int c);
+char *_strcat(char *dest, char *src);
+int _isdigit(int c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strchr(char *s, char c);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+int _islower(int c);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _abs(int num);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+#endif /*MAIN_H*/
